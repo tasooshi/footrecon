0e068cfd8f3b8393cab2ef98203fd7c081eef449
tasooshi <tasooshi@pm.me>
2023-04-14 22:33:41 +0200

    Replaced callback-based audio recording

--- src/footrecon/modules/audio.py
+++ src/footrecon/modules/audio.py
@@ -15,6 +15,7 @@ class Audio(modules.Module):
     output_suffix = '.wav'
     samplerate = 16000
     channels = 1
+    blocksize = 16384
 
     def __init__(self, *args):
         super().__init__(*args)
@@ -39,14 +40,11 @@ class Audio(modules.Module):
             logger.debug('Device not found for {}'.format(self.__class__.__name__))
 
     def task(self, output_file_name, stop_event):
-
-        def callback(indata, frames, time, status):
-            self.queue.put(bytes(indata))
-
         with soundfile.SoundFile(output_file_name, mode='x', samplerate=self.samplerate, subtype='PCM_16', channels=self.channels) as fil:
-            with sounddevice.RawInputStream(samplerate=self.samplerate, blocksize=16384, dtype='int16', device=self.device, channels=self.channels, callback=callback) as in_stream:
-                while not self.queue.empty():
-                    indata = self.queue.get()
-                    fil.buffer_write(indata, dtype=in_stream.dtype)
+            with sounddevice.RawInputStream(samplerate=self.samplerate, blocksize=self.blocksize, dtype='int16', device=self.device, channels=self.channels) as in_stream:
+                while True:
+                    sounddevice.sleep(int(1000 * self.blocksize / self.samplerate))
+                    in_data, _ = in_stream.read(self.blocksize)
+                    fil.buffer_write(in_data, dtype=in_stream.dtype)
                     if stop_event.is_set():
                         break
